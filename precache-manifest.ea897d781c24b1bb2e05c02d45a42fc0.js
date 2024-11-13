6e710bf3c745fe7f8c7cc2810ed47d2f10141fe3
KadogoKenya <catherinekanini001@gmail.com>
2024-11-13 11:26:40 +0300

    Updates

---
+++ precache-manifest.ea897d781c24b1bb2e05c02d45a42fc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86eac669c807ae97f177a99e70902fb2",
+    "url": "/shoppingcart/index.html"
+  },
+  {
+    "revision": "6f5ec7ed13ee344439d7",
+    "url": "/shoppingcart/static/css/2.876f1645.chunk.css"
+  },
+  {
+    "revision": "d26939436ac72c7aeea3",
+    "url": "/shoppingcart/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "6f5ec7ed13ee344439d7",
+    "url": "/shoppingcart/static/js/2.b934a882.chunk.js"
+  },
+  {
+    "revision": "12adf5ee6e135646e379",
+    "url": "/shoppingcart/static/js/3.8ba39429.chunk.js"
+  },
+  {
+    "revision": "d26939436ac72c7aeea3",
+    "url": "/shoppingcart/static/js/main.edc38c19.chunk.js"
+  },
+  {
+    "revision": "e0b2b83e97cc2e327245",
+    "url": "/shoppingcart/static/js/runtime~main.287509cf.js"
+  }
+]);
